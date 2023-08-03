f715a9d83e2ac06c796ccb0da206f682d426f9ed
Aaron <aaronromeromusic@gmail.com>
2023-08-03 20:27:31 +0200

    v2

--- react-project-mern1-aaron/src/pages/About.jsx
+++ react-project-mern1-aaron/src/pages/About.jsx
@@ -4,7 +4,7 @@ export const About = () =>{
         <>
             <h1 className="title">ABOUT</h1>
             <p className="description">¿QUIENES SOMOS?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat esse, rem deleniti optio ipsa eum atque nostrum odit et cumque officiis? Est consequatur ullam aut. Ipsum doloribus ipsa et possimus.</p>
+            <p>En NASA, nos tomamos muy en serio a las personas con inquietudes y con ganas de chafardear el más allá. Por eso, desde el más acá, te traemos lo que nuestros satélites, sondas, rovers, cámaras hyper caras, hacen por ti y por todos. Nos complace que puedas stalkear el espacio despacio, y por ello, te brindamos una amplia selección de imagenes que podrás buscar por fecha, para que cada día puedas despertarte y descubrir un nuevo mundo, el cual esperamos que te brinde la oportunidad de animarte a descubrir el tuyo propio en el planeta, y des rienda suelta a tu imaginación.</p>
         </>
         )
 }
