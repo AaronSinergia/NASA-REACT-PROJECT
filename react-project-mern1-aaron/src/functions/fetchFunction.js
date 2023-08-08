d7a9bc9f2b53737382c9c52376fd37d9b136c51c
Aaron <aaronromeromusic@gmail.com>
2023-08-08 19:26:38 +0200

    sigo haciendo pruebas, no funciona

--- react-project-mern1-aaron/src/functions/fetchFunction.js
+++ react-project-mern1-aaron/src/functions/fetchFunction.js
@@ -4,7 +4,7 @@ export const ApiPetition = ({ isMars, stateToChange, selectedDate }) => {
   const NASA_API = 'https://api.nasa.gov/';
   const NASA_API_KEY = 'Cv26rEXaDRpS2GDm5J2E2qYKXpGWT7O4Tn6YchRu';
   const APOD_URL = `${NASA_API}planetary/apod?date=${today}&api_key=${NASA_API_KEY}`;
-  const MARS_URL = `${NASA_API}planetary/apod?date=${
+  const MARS_URL = `${NASA_API}mars-photos/api/v1/rovers/curiosity/photos?sol=1000${
     selectedDate ? selectedDate : today
   }&api_key=${NASA_API_KEY}`;
   const MARS_URL_DAY = `${NASA_API}mars-photos/api/v1/rovers/curiosity/photos?earth_date=${selectedDate}&api_key=${NASA_API_KEY}`;
